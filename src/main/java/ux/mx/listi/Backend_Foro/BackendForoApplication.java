ed90a74295d14c78b33ddfb3927fc65248c9a2e2
Vsdestroyer0 <ggmendoza2005@gmail.com>
2025-11-27 11:16:40 -0600

    Inicio Backend

---
+++ src/main/java/ux/mx/listi/Backend_Foro/BackendForoApplication.java
@@ -0,0 +1,13 @@
+package ux.mx.listi.Backend_Foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ux/mx/listi/Backend_Foro/BackendForoApplicationTests.java
@@ -0,0 +1,13 @@
+package ux.mx.listi.Backend_Foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
