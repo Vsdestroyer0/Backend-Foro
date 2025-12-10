f777f1dc1bba229e3d526ed5f472f8f567f126e5
Emir Tirso <turumbar451@hotmail.com>
2025-12-10 06:33:36 -0600

    se agrego cors y se arreglo servicio de dar like

--- src/main/java/ux/mx/listi/Backend_Foro/BackendForoApplication.java
+++ src/main/java/ux/mx/listi/Backend_Foro/BackendForoApplication.java
@@ -1,5 +1,8 @@
 package ux.mx.listi.Backend_Foro;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +14,19 @@ public class BackendForoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendForoApplication.class, args);
 	}
+
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*") // permite acceso desde cualquier URL 
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
+        }
+    };
+}
 
 }
 
